d21474a49198a0fa146ecee57b2193ccc45debc8
Ajinkya Tilekar <tilekarajinkya2001@gmail.com>
2024-01-26 15:43:57 +0530

    Add project files.

---
+++ PMS.API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PMS.API.Data;
+
+namespace PMS.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly PMSDbContext _pmsDbContext;
+        public ProductsController(PMSDbContext pmsDbContext)
+        {
+            _pmsDbContext = pmsDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
