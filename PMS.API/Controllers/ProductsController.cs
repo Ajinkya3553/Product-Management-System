35252813b004c4403153d4ccb3a764c72b7ae1a8
Ajinkya Tilekar <tilekarajinkya2001@gmail.com>
2024-01-30 17:34:14 +0530

    GetAllProducts, AddProduct, GetProduct methods added. UseCors added.

--- PMS.API/Controllers/ProductsController.cs
+++ PMS.API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PMS.API.Data;
+using PMS.API.Models;
 
 namespace PMS.API.Controllers
 {
@@ -13,9 +15,31 @@ namespace PMS.API.Controllers
             _pmsDbContext = pmsDbContext;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
         {
-            return View();
+            var products = await _pmsDbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] Product product)
+        {
+            product.Id = Guid.NewGuid();
+            await _pmsDbContext.Products.AddAsync(product);
+            await _pmsDbContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await _pmsDbContext.Products.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (product == null)
+                return NotFound();
+            return Ok(product);
         }
     }
 }
